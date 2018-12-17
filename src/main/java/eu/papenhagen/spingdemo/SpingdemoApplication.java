f8cf381a320bb938b88251749165c23678b31802
jenspapenhagen <jens.papenhagen@web.de>
2018-12-17 12:25:40 +0100

    first init

---
+++ src/main/java/eu/papenhagen/spingdemo/SpingdemoApplication.java
@@ -0,0 +1,14 @@
+package eu.papenhagen.spingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingdemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/eu/papenhagen/spingdemo/SpingdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package eu.papenhagen.spingdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
